e02676ad5405b7a96987aa7d487b784d7fcd2f71
Jiantong-Wang <55922554+Jiantong-Wang@users.noreply.github.com>
2019-12-08 19:57:09 -0600

    Upload Split.sh

---
+++ projectCHTC/split_2017.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#910227 line in 2014.csv
+#11809234 line in 2015.csv
+#10626900 line in 2016.csv
+#10803029 line in 2017.csv
+
+split -d -l 216061 2017/2017.csv
+rm -rf 2017/2017.csv
+cat x00 | awk 'NR>1' > x00new
+rm -rf x00
+mv x00new x00
+
